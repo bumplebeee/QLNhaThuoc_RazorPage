30e737c6e6bdf7a92320743a0eebfbddb55413af
Tran Ngo Hung Vi <vitnhhe173419@fpt.edu>
2025-03-29 18:40:25 +0700

    update: edit Thuoc

--- QLThuoc/Pages/Pharmacy/View_Thuoc.cshtml.cs
+++ QLThuoc/Pages/Pharmacy/View_Thuoc.cshtml.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using QLThuoc.Models;
-using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace QLThuoc.Pages.Pharmacy
 {
@@ -15,15 +16,46 @@ namespace QLThuoc.Pages.Pharmacy
             _context = context;
         }
 
-        public List<Thuoc> DanhSachThuoc { get; set; }
+        // Danh sách thuốc hiển thị trong bảng
+        public List<Thuoc> DanhSachThuoc { get; set; } = new List<Thuoc>();
+
+        // Thuộc tính binding cho form cập nhật
+        [BindProperty]
+        public Thuoc ThuocUpdate { get; set; } = new Thuoc();
 
         public async Task OnGetAsync()
         {
             DanhSachThuoc = await _context.Thuocs
-                .Include(t => t.IdDmNavigation)  // Tham chiếu đến bảng DanhMuc
-                .Include(t => t.IdXxNavigation)  // Tham chiếu đến bảng XuatXu
-                .Include(t => t.IdDvtNavigation) // Tham chiếu đến bảng DonViTinh
+                .Include(t => t.IdDmNavigation)
+                .Include(t => t.IdXxNavigation)
+                .Include(t => t.IdDvtNavigation)
                 .ToListAsync();
+        }
+
+        // Handler cập nhật dữ liệu khi form được submit
+        public async Task<IActionResult> OnPostUpdateThuocAsync()
+        {
+            var existingThuoc = await _context.Thuocs.FindAsync(ThuocUpdate.IdThuoc);
+            if (existingThuoc == null)
+            {
+                return NotFound("Không tìm thấy thuốc");
+            }
+
+            // Cập nhật các trường (lưu ý: các giá trị cần phải là ID thay vì tên nếu bạn cập nhật khóa ngoại)
+            existingThuoc.TenThuoc = ThuocUpdate.TenThuoc;
+            existingThuoc.IdDm = ThuocUpdate.IdDm;
+            existingThuoc.IdXx = ThuocUpdate.IdXx;
+            existingThuoc.IdDvt = ThuocUpdate.IdDvt;
+            existingThuoc.SoLuongTon = ThuocUpdate.SoLuongTon;
+            existingThuoc.GiaNhap = ThuocUpdate.GiaNhap;
+            existingThuoc.DonGia = ThuocUpdate.DonGia;
+            existingThuoc.ThanhPhan = ThuocUpdate.ThanhPhan;
+
+            _context.Thuocs.Update(existingThuoc);
+            await _context.SaveChangesAsync();
+
+            TempData["Message"] = "Cập nhật thành công";
+            return RedirectToPage();
         }
     }
 }
